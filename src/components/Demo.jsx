a5dea5013386314eeb67071fb059c423fa74f521
Towett <towettf@yahoo.com>
2024-01-26 21:09:40 -0600

    API request and handle errors

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,27 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from '../assets';
 
+import { useLazyGetSummaryQuery } from '../services/article';
+
 const Demo = () => {
-  const [article, setArticle] = useState ({
-    url: '',
-    summary: '',
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
   });
 
+const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
   const handleSubmit = async (e) => {
-    alert('Submitted');
+
+    e.preventDefault();
+
+    const { data } = await getSummary ({ articleUrl: article.url});
+    if (data?.summary) {
+      const newArticle = {...article, summary: data.summary};
+
+      setArticle(newArticle);
+    }
   }
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -24,19 +36,23 @@ const Demo = () => {
           alt="link_icon"
           className="absolute left-0 my-2 ml-3 w-5" 
           />
-          <input 
-          type="url"
-          placeholder="Enter a URL" 
-          value={article.url}
-          onChange={(e) => setArticle({...article, url: e.target.value})}
-          required
-          className="url_input peer"
+
+          <input
+            type="url"
+            placeholder="Paste the URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer" // When you need to style an element based on the state of a 
+            //sibling element, mark the sibling with the peer class, and use peer-* 
+            //modifiers to style the target element
           />
+
           <button
           type="submit"
-          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700" //here is where peer is modified
           >
-            Go
+            <p>Go</p>
           </button>
         </form>
         {/* Browse URL History */}
