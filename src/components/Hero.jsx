10c5c4aa2c49e8200cc7f81b614ffdb237a41e56
Towett <towettf@yahoo.com>
2025-04-12 00:52:00 -0500

    updated the name of the app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
       <h1 className="head_text">Summarize Articles with <br className="max-md:hidden" />
       <span className="orange_gradient">OpenAI GPT-4</span></h1>
       <h2 className="desc"> 
-      Simplify your reading with Sumzer. Sumzer Transforms lengthy articles into clear and concise summaries.
+      Simplify your reading with Sumz. Sumz Transforms lengthy articles into clear and concise summaries.
       </h2>
     </header>
   )
