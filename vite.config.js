99c79f8d36f494a8bae82e2791da23c53e0892e1
Towett <towettf@yahoo.com>
2025-04-12 11:49:44 -0500

    updated package.json file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { vitePlugin as remix } from "@remix-run/dev";
-// ↓ add this
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    remix(),
-    netlifyPlugin()
-  ],
+  plugins: [react() ],
 })
